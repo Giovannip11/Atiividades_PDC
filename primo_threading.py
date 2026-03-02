88268ce8788b8bcf0855add39bd81211127713ed
Giovannip11 <giggiopinto@gmail.com>
2026-03-02 10:25:07 -0300

    Estrutura básica para cálculo de números primos com threading.

--- primo_threading.py
+++ primo_threading.py
@@ -2,17 +2,32 @@ import threading
 import time
 from math import sqrt
 
+
 def eh_primo(n):
-    if n < n:
+    if n < 2:
         return False
-    if n==2:
+    if n == 2:
         return n
-    if n%2==0:
+    if n % 2 == 0:
         return False
 
-    limit = int(sqrt(n+1))
-    for i in range(3,limit,2):
-        if n % i==0:
+    limit = int(sqrt(n + 1))
+    for i in range(3, limit, 2):
+        if n % i == 0:
             return False
-    
+
     return n
+
+
+class MyThread(threading.Thread):
+    def __init__(self, inicio, fim, resultados):
+        threading.Thread.__init__(self)
+        self.inicio = inicio
+        self.fim = fim
+        self.resultados = resultados
+
+    def run(self):
+        print("Iniciando Thread: %s." % self.name)
+        for n in range(self.inicio, self.fim + 1):
+            if eh_primo(n):
+                self.resultados.append(n)
