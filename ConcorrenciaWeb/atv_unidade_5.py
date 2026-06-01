f2225bc957c01fdfd66c705e365e5a25375c567f
Giovanni Milan <125603962+Giovannip11@users.noreply.github.com>
2026-06-01 08:30:04 -0300

    Create atv_unidade_5.py

---
+++ ConcorrenciaWeb/atv_unidade_5.py
@@ -0,0 +1,39 @@
+import threading
+import requests
+import time
+
+class MyThread(threading.Thread):
+    def __init__(self, url):
+        threading.Thread.__init__(self)
+        self.url = url
+
+        self.result = None
+
+    def run(self):
+        res = requests.get(self.url)
+        self.result = f'{self.url}: {res.text}'
+
+urls = [
+     'http://httpstat.us/200',
+     'http://httpstat.us/200?sleep=20000',
+     'http://httpstat.us/400',
+     'http://httpstat.us/404',
+     'http://httpstat.us/408?sleep=5000',
+     'http://httpstat.us/418',
+     'http://httpstat.us/500?sleep=2000',
+     'http://httpstat.us/524'
+    ]
+
+start =  time.time()
+
+threads = [MyThread(url) for url in urls]
+
+for thread in threads:
+    thread.join()
+
+for thread in threads:
+    print(thread.result)
+
+print(f'Threading {time.time() - start : .2f} seconds')
+
+print('Done')
