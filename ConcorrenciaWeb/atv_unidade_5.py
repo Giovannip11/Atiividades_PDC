c6089f05e5394cec9945ac3dcf3a3231c1230260
Giovannip11 <giggiopinto@gmail.com>
2026-06-02 17:07:25 -0300

    atualizaçao

--- ConcorrenciaWeb/atv_unidade_5.py
+++ ConcorrenciaWeb/atv_unidade_5.py
@@ -1,6 +1,8 @@
 import threading
-import requests
 import time
+
+import requests
+
 
 class MyThread(threading.Thread):
     def __init__(self, url):
@@ -11,29 +13,36 @@ class MyThread(threading.Thread):
 
     def run(self):
         res = requests.get(self.url)
-        self.result = f'{self.url}: {res.text}'
+        self.result = f"{self.url}: {res.text}"
+
 
 urls = [
-     'http://httpstat.us/200',
-     'http://httpstat.us/200?sleep=20000',
-     'http://httpstat.us/400',
-     'http://httpstat.us/404',
-     'http://httpstat.us/408?sleep=5000',
-     'http://httpstat.us/418',
-     'http://httpstat.us/500?sleep=2000',
-     'http://httpstat.us/524'
-    ]
+    "http://httpstat.us/200",
+    "http://httpstat.us/200?sleep=20000",
+    "http://httpstat.us/400",
+    "http://httpstat.us/404",
+    "http://httpstat.us/408?sleep=5000",
+    "http://httpstat.us/418",
+    "http://httpstat.us/500?sleep=2000",
+    "http://httpstat.us/524",
+]
 
-start =  time.time()
+start = time.time()
+
 
 threads = [MyThread(url) for url in urls]
+
+
+for thread in threads:
+    thread.start()
+
 
 for thread in threads:
     thread.join()
 
+
 for thread in threads:
     print(thread.result)
 
-print(f'Threading {time.time() - start : .2f} seconds')
-
-print('Done')
+print(f"Threading {time.time() - start: .2f} seconds")
+print("Done")
